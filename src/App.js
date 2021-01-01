e29633497dd6b53262da6ec4e8a912fa58d83733
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2021-01-01 22:14:28 +0600

    Using API USA Breaking News

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
 import './App.css';
+import News from './Component/News/News'
+import React, { useEffect, useState } from 'react';
+import Header from './Component/Header/Header';
 
 function App() {
+  const [articles , setArticles] = useState([]);
+  useEffect( () =>{
+     const url ="http://newsapi.org/v2/top-headlines?country=us&apiKey=f5fe2bccfe4948f6aadfea6c97ab13bf"
+      fetch(url)
+      .then (res=> res.json())
+      .then (data => setArticles(data.articles))
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header></Header>
+       <h4>   Breaking News Top {articles.length}</h4>
+       {
+         articles.map(article =><News article={article}></News>)
+       }
     </div>
   );
 }
